990317ef0fa4c03fe737b93a51ab3ba7e5987e47
ThienAnTran-Skedulo <an.tran@skedulo.com>
2021-09-04 16:08:46 +0700

    [Home] Create table of books

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import NavBar from './Components/NavBar/NavBar';
+import Home from './Components/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Home />
     </div>
   );
 }
